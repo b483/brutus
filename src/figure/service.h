8e0f9ca630bb5b51f4484a8c4fda56fd822d9385
b483 <26310843+b483@users.noreply.github.com>
2023-09-09 13:13:42 +0100

    Merge all files into one
    Merge all remaining source files into one file.
    Change game settings and configs file format from binary to .txt.

--- src/figure/service.h
+++
@@ -1,8 +0,0 @@
-#ifndef FIGURE_SERVICE_H
-#define FIGURE_SERVICE_H
-
-#include "figure/figure.h"
-
-int figure_service_provide_coverage(struct figure_t *f);
-
-#endif // FIGURE_SERVICE_H
--- src/platform/brutus.h
+++ src/platform/brutus.h
@@ -1,40 +1,6 @@
 #ifndef PLATFORM_BRUTUS_H
 #define PLATFORM_BRUTUS_H
 
-#include "input/input.h"
-
-#include "SDL.h"
-
-enum {
-    USER_EVENT_QUIT,
-    USER_EVENT_RESIZE,
-    USER_EVENT_FULLSCREEN,
-    USER_EVENT_WINDOWED,
-    USER_EVENT_CENTER_WINDOW,
-};
-
-const char *system_version(void);
-
-void system_resize(int width, int height);
-
-int scale_display(int scale_percentage);
-
-int get_max_display_scale(void);
-
-void init_cursors(int scale_percentage);
-
-void set_cursor(int cursor_id);
-
-void system_keyboard_set_input_rect(int x, int y, int width, int height);
-
-void set_relative_mouse_mode(int enabled);
-
-void post_event(int code);
-
-void log_info(const char *msg, const char *param_str, int param_int);
-
-void log_error(const char *msg, const char *param_str, int param_int);
-
-void update_screen(void);
+  char *system_version(void);
 
 #endif // GAME_SYSTEM_H
